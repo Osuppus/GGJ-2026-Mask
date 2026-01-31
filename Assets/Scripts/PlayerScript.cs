da1b5227cc2f540c92db981699dbd8f974a32f30
Lelainep2003 <lelaineperez@gmail.com>
2026-01-31 14:39:34 -0500

    Basic Player Movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,33 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D RB;
+    public float speed = 5f;
+
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // HORIZONTAL + VERTICAL MOVEMENT works both WASD + Arrows
+        Vector2 vel = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            vel.x = speed;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            vel.x = -speed;
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            vel.y = speed;
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            vel.y = -speed;
+
+        RB.linearVelocity = vel;
     }
 }
+
